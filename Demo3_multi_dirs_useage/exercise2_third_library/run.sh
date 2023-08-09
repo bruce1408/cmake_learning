a00e551d57c5d17de7b8bd5fcaeb2ff82b094b4d
bruce1408 <summer56567@163.com>
2023-08-09 16:11:57 +0800

    update

--- Demo3_multi_dirs_useage/exercise2_third_library/run.sh
+++ Demo3_multi_dirs_useage/exercise2_third_library/run.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-cd /Users/bruce/CppProjects/cmake_learning/Demo3_multi_dirs_useage/exercise2_third_library
+cd /Users/bruce/CppProjects/CPlusPlusThings/extensions/cmake_learning/Demo3_multi_dirs_useage/exercise2_third_library
 
 rm -rf build/
 echo "create build dir..."
 mkdir build
 echo "build dir is generate done"
 cd build/
-echo "do:cmake"
+echo "========== begin to exec cmake"
 cmake ../
-echo "do the next step: make"
+echo "========== exec the next step: make"
 make
 
 echo "Run Demo"
