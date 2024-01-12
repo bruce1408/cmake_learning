929b6bb19249c68c457b9677cd1c25b2be1bcf2e
bruce1408 <summer56567@163.com>
2024-01-12 16:04:27 +0800

    fix(class): add class namespace
    添加命名空间

--- Demo2_multi_files_usage/exercise1/run.sh
+++ Demo2_multi_files_usage/exercise1/run.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 
+info_p() {
+    local message=$1
+    local progress=$2
+    local color='\033[1;32m'
+    echo -e "$color[INFO][$progress%]$message\033[0m"
+}
+
+info_err() {
+    local message=$1
+    # local progress=$2
+    local color='\033[1;31m'
+    echo -e "$color[ERROR]$message\033[0m"
+}
+
+# 普通打印信息
+info() {
+    local message=$1
+    local progress=$2
+    local color='\033[1;34m'
+    echo -e "$color$message\033[0m"
+}
+
+
 workdir=$(cd $(dirname $0); pwd)
 
 
